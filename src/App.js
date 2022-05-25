7153e6e1532c05a5de3315ef66a9998e4489907c
razan salah <razansalah022@gmail.com>
2022-05-25 07:47:32 +0200

    deploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router basename={window.location.pathname || ""}>
       <div className="App">
         <Switch>
           <Route exact path="/">
