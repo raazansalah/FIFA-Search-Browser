58dc1454b8774b4823ce74592f678698d4cf1f8b
razan salah <razansalah022@gmail.com>
2022-05-25 07:38:30 +0200

    deploy

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
+import "./App.css";
+import { PlayerProfile } from "./components/PlayerProfile";
+import { SearchBar } from "./components/SearchBar";
+import playerData from "./data.json";
 
-import './App.css';
-import { PlayerProfile } from './components/PlayerProfile';
-import { SearchBar } from './components/SearchBar';
-import playerData from "./data.json"
-
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
-
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Switch>
           <Route exact path="/">
-          <SearchBar data={playerData} />
+            <SearchBar data={playerData} />
           </Route>
-          <Route  path="/playerprofile/:Name/:id">
-          <PlayerProfile />
+          <Route path="/playerprofile/:Name/:id">
+            <PlayerProfile />
           </Route>
         </Switch>
-
       </div>
     </Router>
   );
